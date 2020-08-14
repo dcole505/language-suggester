9930117be2361b4be30890033976127fdce1e5ba
David Cole <davidtcole@yahoo.com>
2020-08-14 14:59:03 -0700

    add JQuery to the UI logic to capture the value of the form submissions into an array, along with a separate variable to store the user name input. Additionally added functionality to toggle between the survey form when the button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,16 @@
 $(document).ready(function() {
   $("#form-reveal").click(function(event){
     event.preventDefault();
+    const answers = [
+      $("#question-1").val(),
+      $("#question-2").val(),
+      $("#question-3").val(),
+      $("#question-4").val(),
+      $("#question-5").val(),
+    ];
+    const name = $("#name").val();
 
-    $("#survey").show();
-    $("#display").hide();
-  })
+    $("#survey").toggle();
+    $("#display").toggle();
+  });
 })
